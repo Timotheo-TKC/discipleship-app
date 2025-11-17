02de052096ee69c6a162f6c0eedd5f543d13b7d5
SingasonSimon <simonnjorogew65@gmail.com>
2025-11-17 19:46:58 +0300

    Refactor CustomMessageNotification class by removing Queueable trait and ShouldQueue interface, simplifying the notification structure.

--- app/Notifications/CustomMessageNotification.php
+++ app/Notifications/CustomMessageNotification.php
@@ -2,14 +2,11 @@
 
 namespace App\Notifications;
 
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class CustomMessageNotification extends Notification implements ShouldQueue
+class CustomMessageNotification extends Notification
 {
-    use Queueable;
 
     public function __construct(
         public string $subject,
